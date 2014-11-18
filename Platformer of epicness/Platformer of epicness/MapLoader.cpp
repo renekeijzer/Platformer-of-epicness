fbe8bf5acd7bdc00911c9cfa03b7966d69084ab9
renekeijzer <rene.keijzer@casema.nl>
2014-11-18 13:02:46 +0100

    Maploading started

--- Platformer of epicness/Platformer of epicness/MapLoader.cpp
+++ Platformer of epicness/Platformer of epicness/MapLoader.cpp
@@ -1,11 +1,103 @@
 #include "MapLoader.hpp"
 
+MapLoader* MapLoader::instance = nullptr;
 
-MapLoader::MapLoader()
-{
+MapLoader* MapLoader::get(){
+	if (!instance){
+		instance = new MapLoader;
+		instance->parseXml();
+	}
+	return instance;
 }
 
+void MapLoader::parseXml(){
+	std::ifstream levelMapping("Resources/Levels.dat");
+	char ch;
+	char buffer[65536];
+	size_t chars_read = 0;
 
-MapLoader::~MapLoader()
-{
+	while (levelMapping.get(ch) && (chars_read < 65535)){
+		buffer[chars_read++] = ch;
+	}
+	buffer[chars_read++] = '\0';
+
+	rapidxml::xml_document<> map;
+	map.parse<0>(buffer);
+	rapidxml::xml_node<> *startMapping = map.first_node("levels")->first_node();
+	try{
+		while (startMapping != 0){
+			int id = atoi(startMapping->first_attribute("id")->value();
+			
+			level tmpLevel;
+			tmpLevel.source = startMapping->first_attribute("source")->value();
+			tmpLevel.data = "";
+			tmpLevel.spritesheet = startMapping->first_attribute("spritesheet")->value();
+			tmpLevel.next = nullptr;
+
+			if (id != 1){ levels[id - 1]->next = &tmpLevel; }
+			levels[id] = &tmpLevel;
+
+			startMapping = startMapping->next_sibling();
+		}
+	}
+	catch (rapidxml::parse_error &ex){
+		std::cout << ex.what();
+	}
+	levelMapping.close();
 }
+
+void MapLoader::createMap(GameScreen* screen){
+	try{
+		if (!current){ 
+			current = levels[1]; 
+		}
+		else
+		{
+			current = current->next;
+		}
+
+		std::ifstream levelFile(current->source);
+		char ch;
+		char buffer[65536];
+		size_t chars_read = 0;
+
+		while (levelFile.get(ch) && (chars_read < 65535)){
+			buffer[chars_read++] = ch;
+		}
+		buffer[chars_read++] = '\0';
+
+		rapidxml::xml_document<> map;
+		map.parse<0>(buffer);
+		rapidxml::xml_node<> *startMapping = map.first_node("map")->first_node();
+		while (startMapping != 0){
+
+		}
+
+}
+
+std::vector<std::string> MapLoader::explode(const std::string& str, const char& ch) {
+	std::string next;
+	std::vector<std::string> result;
+
+	// For each character in the string
+	for (std::string::const_iterator it = str.begin(); it != str.end(); it++) {
+		// If we've hit the terminal character
+		if (*it == ch) {
+			// If we have some characters accumulated
+			if (!next.empty()) {
+				// Add them to the result vector
+				result.push_back(next);
+				next.clear();
+			}
+		}
+		else {
+			// Accumulate the next character into the sequence
+			next += *it;
+		}
+	}
+	if (!next.empty())
+		result.push_back(next);
+	return result;
+}
+
+MapLoader::~MapLoader(){}
--- Platformer of epicness/Platformer of epicness/MapLoader.hpp
+++ Platformer of epicness/Platformer of epicness/MapLoader.hpp
@@ -1,8 +1,58 @@
+
 #pragma once
-class MapLoader
-{
+#include <map>
+#include <vector>
+#include "Screen.hpp"
+#include "GameScreen.hpp"
+#include <iostream>
+#include <fstream>
+#include "rapidxml.hpp"
+#include "rapidxml_iterators.hpp"
+#include "rapidxml_utils.hpp"
+#include "rapidxml_print.hpp"
+#include <string>
+#include "Layer.hpp"
+#include <SFML\Graphics.hpp>
+#include <vector>
+#include "TextureManager.hpp"
+
+
+
+class GameScreen;
+
+//! \brief This class will make all the tilecomponents for the levels
+class MapLoader{
 public:
-	MapLoader();
+	static MapLoader* get();
+
+
+	//! \brief Will add all tiles to components list, and will be drawn when game loop is initiated
+	//! \param screen screen on which the objects will be added
+	void createMap(GameScreen* screen);
+
+	//! \brief Resets all values to their defualt values
+	void resetLevelNr();
 	~MapLoader();
+
+private:
+	//! \brief initiating maploader
+	MapLoader(){};
+
+	//! \brief Will fill the tileMapping with <id, passable>
+	void parseXml();
+
+	//! \brief works like php explode
+	//! \param str string to explode
+	//! \param ch delimeter character
+	std::vector<std::string> MapLoader::explode(const std::string& str, const char& ch);
+
+	std::map<int, std::string> tileMapping;
+	int tileWidth, tileHeight, spritesheetWidth, spritesheetHeight;
+	
+	static MapLoader* instance;
+	typedef struct level{ std::string source; std::string spritesheet; std::string data; struct level* next; }level;
+	std::map<int, struct level*> levels;
+	level * current = nullptr;
+
 };
 
